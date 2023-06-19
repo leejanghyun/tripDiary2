58209fe83e3aef187483122084bba56ee01e0706
화물개발 이장헌 <morison@tmapmobility.com>
2023-06-19 21:04:27 +0900

    deploy 수정

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 echo "> FE 배포"
 cd /home/ubuntu/deploy/next-app
+pm2 delete tripDiary
 yarn install
 yarn build
+pm2 start yarn --name "tripDiary" -- start
--- standalone.config.js
+++ standalone.config.js
@@ -1,22 +1,22 @@
 module.exports = {
   apps: [
     {
-      name: "tripDiary",
-      script: "yarn",
-      args: "run start",
+      name: 'tripDiary',
+      script: 'yarn',
+      args: 'run start',
       instances: 1,
       autorestart: true,
       watch: false,
       env: {
         PORT: 443,
-        NODE_ENV: "development",
+        NODE_ENV: 'development',
       },
       env_production: {
         PORT: 443,
-        NODE_ENV: "production",
+        NODE_ENV: 'production',
       },
-      output: "./logs/console.log",
-      error: "./logs/consoleError.log",
+      output: './logs/console.log',
+      error: './logs/consoleError.log',
     },
   ],
-};
+}
