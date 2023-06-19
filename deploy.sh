08d9d72c13a1ed37954251d21beff7b217ac3c3f
화물개발 이장헌 <morison@tmapmobility.com>
2023-06-19 21:54:34 +0900

    script 추가

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 echo "> FE 배포"
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g pm2
 cd /home/ubuntu/deploy/next-app
 yarn install
 pm2 stop tripDiary
